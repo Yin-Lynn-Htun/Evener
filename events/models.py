4fa7d244dd21b25199fb50998300f58bebbe3405
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-28 17:14:09 +0630

    Added admin and models

--- events/models.py
+++ events/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     name = models.CharField(max_length=100)
-    registered = models.IntegerField()
+    location = models.CharField(max_length=255)
+    date = models.DateField()
+    registered = models.IntegerField(default=0)
+    organizer = models.CharField(max_length=100, default='Maung Maung')
+    detail = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.name 
